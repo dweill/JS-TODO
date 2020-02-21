91a554a6d4302ca5c7d3fecc0734d4da4a1e5a43
Daniel Weill <daniel.zalmen@gmail.com>
2020-02-20 20:24:23 -0600

    add skeleton of add function

---
+++ index.js
@@ -0,0 +1,7 @@
+//  Initialize variable tasks to empty array
+
+// Define add task function takes a task name
+  // Check the length of tasks
+    // If tasks has length
+      // Set todo list visibility to true
+    // Add table row with task name value
