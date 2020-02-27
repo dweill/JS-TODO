97fca66b5aee3a7f021b280a85decb9643114497
Daniel Weill <daniel.zalmen@gmail.com>
2020-02-26 18:26:08 -0600

    modify logic to ignore visibility

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 //  Initialize variable tasks to empty array
 
 // Define add task function takes a task name
-  // Check the length of tasks
-    // If tasks has length
-      // Set todo list visibility to true
-    // Add list item with task name value
+    // Get value of taskInput
+    // Add value of taskInput to tasksArray
+    // Add list item to bottom of todolist
+    // Add checkbox to list item
+    // Add value to list item
