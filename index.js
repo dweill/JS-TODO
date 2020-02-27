5641c4724ffc872eabb9fca59af2c9745419ed2a
Daniel Weill <daniel.zalmen@gmail.com>
2020-02-26 18:50:15 -0600

    refactor comments

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 //  Initialize variable tasks to empty array
 
 // Define add task function takes a task name
-    // Get value of taskInput
-    // Add value of taskInput to tasksArray
-    // Add list item to bottom of todolist
-    // Add checkbox to list item
-    // Add value to list item
+  // Add taskName to tasksArray
+  // Create list item
+  // Set list item id to taskName for reference
+  // Add list item to bottom of todolist
+  // Create input
+  // Set the type of the input to checkbox
+  // Add checkbox to list item
+  // Create span
+  // Set value of span to taskName
+  // Add value to list item
+
+// Get add task button
+// add click event listener to add task button
+  // get value of task input
+  // call add task on taskName
