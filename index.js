0622610fcefa3a62f37a04e80aeb3cb092203347
Daniel Weill <daniel.zalmen@gmail.com>
2020-04-05 20:11:51 -0500

    break up reusable logic

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 //  Initialize variable tasks to empty array
+// Initialize variable sequence to 0
 
 // Define add task function takes a task name
-  // Add task name to tasks array
-  // Create list item
-  // Set list item id to task name for reference
+  // Create task object
+  // Add task object to tasks array
+  // Call create list item function
   // Add list item to bottom of todolist
-  // Create input
-  // Set the type of the input to checkbox
-  // Add checkbox to list item
-  // Create span
-  // Set value of span to taskName
+  // Call create checkbox function
+  // Call create span function with task name
   // Add value to list item
 
 // Get add task button
 // Add click event listener to add task button
   // Get value of task input
   // Call add task on taskName
+
+
+// Create define task function that takes a task name
+  // Create and return object with
+    // Id to be incremented as global count
+    // Name key being the task name passed in and 
+    // Complete key set to false
+
+// Create an add list item function
+  // Create list item
+  // Set list item id to task object id for reference
+
+// Create add span item function
+  // Create span
+  // Set value of span to taskName
+
+// Create function for making checkbox input
+  // Set the type of the input to checkbox
+  // Add checkbox to list item
