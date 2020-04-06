874485efd32abd0bdd36e35aecc43c56f7c4f2d9
Daniel Weill <daniel.zalmen@gmail.com>
2020-04-05 20:23:56 -0500

    add complete logic

--- index.js
+++ index.js
@@ -7,9 +7,9 @@
   // Call create list item function
   // Add list item to bottom of todolist
   // Call create checkbox function
+  // Add checkbox to list item
   // Call create span function with task name
   // Add value to list item
-
 // Get add task button
 // Add click event listener to add task button
   // Get value of task input
@@ -32,4 +32,10 @@
 
 // Create function for making checkbox input
   // Set the type of the input to checkbox
-  // Add checkbox to list item
+  // Add click event for completing object to checkbox
+    // Complete becomes the opposite of itself
+    // If complete is true
+      // Add completed class to list item
+    // Else 
+      // Remove completed class from list item
+  
